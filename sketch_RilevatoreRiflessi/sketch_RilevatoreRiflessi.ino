49670a33c54d66373c572d84f14e601d11b7a1c4
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-03-16 09:32:55 +0100

    prove primo e secondo riflesso

--- sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
+++ sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
@@ -1,20 +1,33 @@
 int led1 = 6;
+int ledRosso = 0;
+int ledVerde = 1;
+int ledBlu = 2; 
 int btn0 = 8;
 int btn1 = 10;
+int btn2 = 12;
+int buzzer = 4;
 int tempoLed = 0;
+int tempoBuzzer = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
+  pinMode(ledRosso, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledBlu, OUTPUT);
   pinMode(btn0, INPUT);
   pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   inizio();
   primoRiflesso();
+  secondoRiflesso();
 }
+
 void inizio() {
   while(digitalRead(btn0) != HIGH) {}
 }
@@ -23,9 +36,20 @@ void primoRiflesso() {
   delay(random(2000, 8000));
   digitalWrite(led1, HIGH);
   
-  while (digitalRead(btn1) != HIGH){
+  while (digitalRead(btn1) != HIGH) {
     tempoLed++;
     delay(1);
   }
   digitalWrite(led1, LOW); 
+}
+
+void secondoRiflesso() {
+  delay(random(2000, 8000));
+  digitalWrite(buzzer, HIGH);
+
+  while(digitalRead(btn2) != HIGH) {
+    tempoBuzzer++;
+    delay(1);
+  }
+  digitalWrite(buzzer, LOW);
 }
