72cc7126d813484f97d8215ac0dd4955cf1d4cf5
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-03-16 08:38:23 +0100

    inizio + primo riflesso

--- sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
+++ sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
@@ -1,27 +1,31 @@
-int led1 = 13;
-int btn = 7;
-int val = 0;
-int tempo = 0;
+int led1 = 6;
+int btn0 = 8;
+int btn1 = 10;
+int tempoLed = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
-  pinMode(btn, INPUT);
+  pinMode(btn0, INPUT);
+  pinMode(btn1, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  inizio();
   primoRiflesso();
+}
+void inizio() {
+  while(digitalRead(btn0) != HIGH) {}
 }
 
 void primoRiflesso() {
-  val = digitalRead(btn);
+  delay(random(2000, 8000));
   digitalWrite(led1, HIGH);
-  tempo = random(2000, 5000);
   
-  if (val == HIGH) {
-  digitalWrite(led1, LOW);
-  } else {
-  digitalWrite(led1, HIGH);
+  while (digitalRead(btn1) != HIGH){
+    tempoLed++;
+    delay(1);
   }
+  digitalWrite(led1, LOW); 
 }
