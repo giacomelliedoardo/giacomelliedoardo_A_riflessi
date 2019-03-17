7b949e2c5a0cc9aa7c724b03770065e9b62f25c5
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-03-17 14:05:13 +0100

    funzionante ma senza display

--- sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
+++ sketch_RilevatoreRiflessi/sketch_RilevatoreRiflessi.ino
@@ -1,11 +1,10 @@
-int led1 = 6;
-int ledRosso = 0;
-int ledVerde = 1;
-int ledBlu = 2; 
+int led1 = 7;
+int ledRosso = 2;
+int ledVerde = 4;
 int btn0 = 8;
 int btn1 = 10;
 int btn2 = 12;
-int buzzer = 4;
+int buzzer = 13;
 int tempoLed = 0;
 int tempoBuzzer = 0;
 
@@ -14,7 +13,6 @@ void setup() {
   pinMode(led1, OUTPUT);
   pinMode(ledRosso, OUTPUT);
   pinMode(ledVerde, OUTPUT);
-  pinMode(ledBlu, OUTPUT);
   pinMode(btn0, INPUT);
   pinMode(btn1, INPUT);
   pinMode(btn2, INPUT);
@@ -26,17 +24,22 @@ void loop() {
   inizio();
   primoRiflesso();
   secondoRiflesso();
+  esito();
 }
 
 void inizio() {
   while(digitalRead(btn0) != HIGH) {}
+  tempoLed = 0;
+  tempoBuzzer = 0;
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledRosso, LOW);
 }
 
 void primoRiflesso() {
   delay(random(2000, 8000));
   digitalWrite(led1, HIGH);
   
-  while (digitalRead(btn1) != HIGH) {
+  while (digitalRead(btn1) == LOW) {
     tempoLed++;
     delay(1);
   }
@@ -47,9 +50,18 @@ void secondoRiflesso() {
   delay(random(2000, 8000));
   digitalWrite(buzzer, HIGH);
 
-  while(digitalRead(btn2) != HIGH) {
+  while(digitalRead(btn2) == LOW) {
     tempoBuzzer++;
     delay(1);
   }
   digitalWrite(buzzer, LOW);
+}
+
+void esito() {
+  if (tempoLed <= 500 && tempoBuzzer <= 500) {
+    digitalWrite(ledVerde, HIGH);
+  }
+  else {
+    digitalWrite(ledRosso, HIGH);
+  }
 }
